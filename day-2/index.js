f2e0802a4a22af561ef01dc8b5239a9f011e8d8e
NotJag <j.v.durley@gmail.com>
2024-12-15 22:14:33 +0000

    day 2 part 2

--- day-2/index.js
+++ day-2/index.js
@@ -2,19 +2,53 @@ const input = await Deno.readTextFile("input.txt")
 
 let safeReports = 0
 
-for (const report of input.split("\n")) {
-    const levels = report.split(" ").map((level) => Number(level))
-
+function calculateDifferences(levels) {
     let differences = []
 
     for (let i = 1; i < levels.length; i++) {
         differences.push(levels[i] - levels[i - 1])
     }
 
+    return differences;
+}
+
+function ascendingDescending(differences) {
     const ascending = differences.every((level) => level > 0)
     const descending = differences.every((level) => level < 0)
 
-    safeReports += (ascending || descending) && differences.every(level => Math.abs(level) < 4)
+    return { ascending, descending };
+}
+
+function determineSafeReport(levels) {
+    const differences = calculateDifferences(levels);
+    
+    const { ascending, descending } = ascendingDescending(differences);
+    
+    return (ascending || descending) && differences.every(level => Math.abs(level) < 4)
+}
+
+for (const report of input.split("\n")) {
+    const levels = report.split(" ").map((level) => Number(level))
+
+    let safeReport = determineSafeReport(levels)
+
+    if (!safeReport) {
+        safeReport = [...Array(levels.length).keys()]
+            .map(ix => levels.toSpliced(ix, 1))
+            .some(determineSafeReport)
+
+        // For loop could be used instead
+        
+        // for (const level in levels) {
+        //     const newLevels = levels.toSpliced(level, 1)
+
+        //     safeReport = determineSafeReport(newLevels)
+
+        //     if (safeReport) break;
+        // }
+    }
+
+    safeReports += safeReport
 }
 
 console.log(safeReports)
